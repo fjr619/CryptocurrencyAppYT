b672de977832293cf1103df625357219498468b5
Franky Wijanarko <frank.jr.619@gmail.com>
2021-09-13 20:48:07 +0700

    setting up DI

---
+++ app/src/main/java/com/fjr619/cryptocurrencyappyt/CoinApplication.kt
@@ -0,0 +1,7 @@
+package com.fjr619.cryptocurrencyappyt
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class CoinApplication : Application()
--- app/src/main/java/com/fjr619/cryptocurrencyappyt/common/Constant.kt
+++ app/src/main/java/com/fjr619/cryptocurrencyappyt/common/Constant.kt
@@ -1,4 +1,8 @@
 package com.fjr619.cryptocurrencyappyt.common
 
-object Constant {
+object Constants {
+
+    const val BASE_URL = "https://api.coinpaprika.com/"
+
+    const val PARAM_COIN_ID = "coinId"
 }
